ba52e4b5438f85b26476554ed28fbf6086ba34af
chenliupeng <vclp@163.com>
2021-12-26 02:24:17 +0800

    feat: add

--- app/controller/util.js
+++ app/controller/util.js
@@ -1,21 +1,20 @@
-'use strict';
-const svgCaptcha = require('svg-captcha')
-const Controller = require('egg').Controller;
+"use strict";
+const svgCaptcha = require("svg-captcha");
+const Controller = require("egg").Controller;
 
 class UtilController extends Controller {
   async captcha() {
     const captcha = svgCaptcha.create({
-        size: 4,
-        fontSize: 50,
-        width: 100,
-        height: 40,
-        noise: 3,
-
+      size: 4,
+      fontSize: 50,
+      width: 100,
+      height: 40,
+      noise: 3,
     });
     this.ctx.session.captcha = captcha.text;
     console.log(captcha.text);
-    this.ctx.response.type = "image/svg+xml"
-    this.ctx.body = captcha.data
+    this.ctx.response.type = "image/svg+xml";
+    this.ctx.body = captcha.data;
   }
 }
 
